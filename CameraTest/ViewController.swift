45717ea4621d65ed05a6c89b1a7b7941c6eb4ec6
Kazutaka KAMIYA <cmtest080@gmail.com>
2019-03-18 15:16:39 +0900

    Initial Commit

---
+++ CameraTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraTest
+//
+//  Created by me on 2019/03/18.
+//  Copyright © 2019 Neosystem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
